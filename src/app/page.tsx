490093fa960ffd55347417505db99775e7dfd3ab
kseniyavldk <nunci923@gmail.con>
2024-05-02 11:52:25 +0300

    made navbar and main header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,91 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import React from "react";
+import { useDisclosure } from "@mantine/hooks";
+import {
+  AppShell,
+  Text,
+  SegmentedControl,
+  UnstyledButton,
+  rem,
+  Select,
+} from "@mantine/core";
+import { IconChevronDown } from "@tabler/icons-react";
 
-export default function Home() {
+function Demo() {
+  const [opened, { toggle }] = useDisclosure();
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <AppShell
+      navbar={{
+        width: 300,
+        breakpoint: "sm",
+        collapsed: { mobile: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Navbar p="md" style={{ backgroundColor: "#F2EBF9" }}>
+        <img src="logo.png" alt="Logo" style={{ width: 150 }} />
+        <div style={{ marginTop: "70px" }} />
+        <SegmentedControl
+          orientation="vertical"
+          fullWidth
+          withItemsBorders={false}
+          transitionDuration={500}
+          color="#E5D5FA"
+          style={{
+            textAlign: "left",
+            color: "black",
+            backgroundColor: "#F2EBF9",
+          }}
+          data={["Movies", "Rated movies"]}
         />
-      </div>
+      </AppShell.Navbar>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      <AppShell.Main>
+        <div>
+          <Text
+            style={{
+              fontSize: "32px",
+              fontWeight: 700,
+              lineHeight: "44.8px",
+              textAlign: "left",
+            }}
+          >
+            Movies
+          </Text>
+        </div>
+        <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+          <Select
+            rightSection={
+              <IconChevronDown style={{ width: "1rem", height: "1rem" }} />
+            }
+            label="Genres"
+            placeholder="Select genre"
+            data={[]}
+          />
+          <Select
+            rightSection={
+              <IconChevronDown style={{ width: "1rem", height: "1rem" }} />
+            }
+            label="Release year"
+            placeholder="Select release year"
+            data={[]}
+          />
+          <Select label="Ratings" placeholder="From" data={[]} />
+          <Select placeholder="To" data={[]} />
+          <UnstyledButton>Reset filters</UnstyledButton>
+        </div>
+        <div>
+          <Select
+            rightSection={
+              <IconChevronDown style={{ width: "1rem", height: "1rem" }} />
+            }
+            label="Sort by"
+            data={["Most popular"]}
+          />
+        </div>
+      </AppShell.Main>
+    </AppShell>
   );
 }
+export default Demo;
